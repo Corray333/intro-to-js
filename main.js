308efdea33caf10e93cb22d6dc940d4b3baeeb7c
Corray333 <mark.corray.off@gmail.com>
2024-02-19 10:46:26 +0300

    Added 3D keyboard with social networks

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+
 const tasksNumber = 9
 for (let i = 1; i<=tasksNumber; i++){
     const task_button = document.querySelector(`#task${i}-button`)
@@ -10,7 +14,7 @@ for (let i = 1; i<=tasksNumber; i++){
 
 const time_block = document.querySelector('#time')
 const months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
-const days = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье']
+const days = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота']
 setInterval(() => {
     let time = new Date()
     let date = time.getDate()
@@ -21,7 +25,7 @@ setInterval(() => {
     let minutes = time.getMinutes()
     let seconds = time.getSeconds()
 
-    time_block.innerText = `${date} ${month} ${year}, ${day} : ${hours}/${minutes}/${seconds}`
+    time_block.innerText = `${date} ${month} ${year}, ${days[(day)]} : ${hours}/${minutes}/${seconds}`
 }, 1000)
 
 
@@ -35,6 +39,7 @@ let renderCalendar = (time)=>{
     time.setMonth(month)
     let row = 1
     let column = time.getDay()
+    if (column == 0) column = 7
 
     const monthsNoEnding = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
     const monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
@@ -208,3 +213,171 @@ document.querySelectorAll('.task9 input,textarea').forEach((el)=>{
         eye2.style.transform = `translate(0px, 0px)`;
     })
 })
+
+
+
+// Test
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer.js'
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
+
+
+const renderer = new THREE.WebGLRenderer(
+    {
+        antialias: true,
+        alpha: true
+    }
+)
+renderer.setSize(800, 800)
+
+
+const scene = new THREE.Scene()
+// const geometry = new THREE.SphereGeometry(1, 64, 64)
+// const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 })
+// const cube = new THREE.Mesh(geometry, material)
+// scene.add(cube)
+
+const loader = new GLTFLoader();
+
+
+loader.load( 'assets/base.glb', function ( gltf ) {
+	scene.add( gltf.scene );
+}, undefined, function ( error ) {
+	console.error( error );
+} );
+let gitPushed = false
+loader.load( 'assets/git.glb', function ( gltf ) {
+	scene.add( gltf.scene );
+}, undefined, function ( error ) {
+	console.error( error );
+} );
+loader.load( 'assets/in.glb', function ( gltf ) {
+	scene.add( gltf.scene );
+}, undefined, function ( error ) {
+	console.error( error );
+} );
+loader.load( 'assets/mail.glb', function ( gltf ) {
+	scene.add( gltf.scene );
+}, undefined, function ( error ) {
+	console.error( error );
+} );
+loader.load( 'assets/tg.glb', function ( gltf ) {
+	scene.add( gltf.scene );
+}, undefined, function ( error ) {
+	console.error( error );
+} );
+
+
+
+const pointLight = new THREE.PointLight(0xffffff, 200, 100)
+pointLight.position.set(5, 5, 5)
+scene.add(pointLight)
+
+const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000)
+camera.position.z = 2
+camera.position.x = 2
+camera.position.y = 2
+scene.add(camera)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+controls.update()
+
+const raycaster = new THREE.Raycaster()
+const mouse = new THREE.Vector2()
+
+const transitionDown = (el, from, to, time)=>{
+    let step = (to-from)/time
+    let i = 0
+    let interval = setInterval(() => {
+        el.position.y += step
+        i++
+        if (i>time) {
+            clearInterval(interval)
+            if (from> to) transitionDown(el, el.position.y, from, time)
+        }
+    }, 1);
+}
+let link
+let cPointLabel
+window.addEventListener('click', onClick, false)
+function onClick(event) {
+    const canvas = document.querySelector('canvas');
+    mouse.x = ( (event.clientX - canvas.offsetLeft) / canvas.clientWidth ) * 2 - 1;
+    mouse.y = ( (event.clientY - canvas.offsetTop) / canvas.clientHeight ) * -2 + 1;
+
+    raycaster.setFromCamera(mouse, camera)
+    const intersects = raycaster.intersectObjects(scene.children, true)
+    if (intersects.length == 0) return
+    if (intersects[0].object.userData.name == 'base') {
+        return
+    }
+    if (intersects[0].object.position.y < 0.56) transitionDown(intersects[0].object, intersects[0].object.position.y, intersects[0].object.position.y+0.15, 50)
+    else transitionDown(intersects[0].object, intersects[0].object.position.y, intersects[0].object.position.y-0.15, 50)
+    // Create the HTML content
+    link = document.createElement('a')
+    link.style.fontSize = '20px'
+    link.style.color = 'white'
+    if (cPointLabel != undefined) scene.remove(cPointLabel)
+    cPointLabel = new CSS2DObject(link);
+    switch (intersects[0].object.userData.name) {
+        case 'git':
+            link.href = 'https://github.com/Corray333'
+            link.textContent = 'Мой GitHub'
+            cPointLabel.position.set(1, 0.6, 1)
+            break
+        case 'tg':
+            link.href = 'https://t.me/corray9'
+            link.textContent = 'Мой Telegram'
+            cPointLabel.position.set(-1, 0.6, 1)
+            break
+        case 'in':
+            link.href = 'https://linkedin.com'
+            link.textContent = 'Мой Linkedin'
+            cPointLabel.position.set(-1, 0.6, -1)
+            break
+        case 'mail':
+            link.href = ''
+            link.textContent = 'mark.corray.off@gmail.com'
+            cPointLabel.position.set(1, 0.6, -1)
+            break
+    }
+    scene.add(cPointLabel);
+}
+
+const labelRenderer = new CSS2DRenderer();
+labelRenderer.setSize(800, 800);
+labelRenderer.domElement.style.position = 'absolute';
+labelRenderer.domElement.style.right = '200px';
+document.body.appendChild(labelRenderer.domElement);
+
+
+// HDRI
+const pmremGenerator = new THREE.PMREMGenerator( renderer );
+
+const hdriLoader = new RGBELoader()
+hdriLoader.load( 'assets/ml_gradient_freebie_02.hdr', function ( texture ) {
+  const envMap = pmremGenerator.fromEquirectangular( texture ).texture;
+  texture.dispose(); 
+  scene.environment = envMap
+} );
+
+
+function animate() {
+
+	requestAnimationFrame( animate );
+
+	// required if controls.enableDamping or controls.autoRotate are set to true
+	controls.update();
+
+	renderer.render( scene, camera );
+    labelRenderer.render(scene, camera);
+
+}
+animate()
+
+// renderer.render(scene, camera)
+
+document.body.appendChild(renderer.domElement)
